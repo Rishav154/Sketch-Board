4c0dfe4ae0375e51bb752b69cf6cbe0045d1fd38
Rishav <rishav15045@gmail.com>
2024-10-28 00:10:14 +0530

    Completed the javascript part and now the slider is working functioning correctly

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const gridSide = 600;
+const gridSide = 700;
 let rows = 16;
 let columns = 16;
 
@@ -12,35 +12,39 @@ sketcharea.style.width = `${gridSide}px`;
 sketcharea.style.height = `${gridSide}px`;
 
 function setBackgroundColor() {
-  this.style.backgroundColor = "black";
+    this.style.backgroundColor = "black";
 }
 
-function createGridCells() {
-  const numOfSquares = rows * columns;
-  for (let i = 0; i < rows * columns; i++) {
-    const gridCell = document.createElement("div");
+function createGridCells(gridSize) {
+    rows = columns = gridSize;
+    const numOfSquares = rows * columns;
 
-    gridCell.style.width = `${gridSide / columns - 2}px`;
-    gridCell.style.height = `${gridSide / rows - 2}px`;
-    gridCell.classList.add("cell");
+    removeGridCells();
 
-    sketcharea.appendChild(gridCell);
+    for (let i = 0; i < numOfSquares; i++) {
+        const gridCell = document.createElement("div");
 
-    gridCell.addEventListener("mouseover", setBackgroundColor);
-  }
+        // Adjust for exact pixel calculation and any borders
+        gridCell.style.width = `${gridSide / columns - 2}px`;
+        gridCell.style.height = `${gridSide / rows - 2}px`;
+        gridCell.classList.add("cell");
+
+        sketcharea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", setBackgroundColor);
+    }
 }
 
 function removeGridCells() {
-  while (sketcharea.firstChild) {
-    sketcharea.removeChild(sketcharea.firstChild);
-  }
+    while (sketcharea.firstChild) {
+        sketcharea.removeChild(sketcharea.firstChild);
+    }
 }
 
-slider.oninput = function () {
-  let txt = `${this.value} x ${this.value} (Resolution)`;
-  sliderValue.innerHTML = txt;
-  removeGridCells();
-  createGridCells(this.value);
-};
+createGridCells(slider.value);
 
-createGridCells();
+slider.oninput = function () {
+    let gridSize = this.value;
+    sliderValue.textContent = `${gridSize} x ${gridSize} (Resolution)`;
+    createGridCells(gridSize);
+};
